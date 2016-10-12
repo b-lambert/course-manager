e0bad4d3b9f53a655ff78f548da70a775275273e
b-lambert <benjaminhlambert@gmail.com>
2016-10-12 01:10:43 -0700

    s/index.js/server.js/g

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ var app = express();
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
 
-app.listen(8080, function() {
-
+var listener = app.listen(8080, function() {
+  console.log('Server listening on port ' + listener.address().port);
 });
 
 app.get('/', function(req,res) {
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/courses', function(req,res){
   res.setHeader('Content-Type', 'application/json');
-  res.sendfile('courses.json');
+  res.sendFile(__dirname + '/courses.json');
 });
